1d6bf6041aa380f8adfc8d3a1f92b81babaacbff
Vita <vitalia@myrambler.ru>
2019-09-28 15:15:29 +0300

    changed to function components

--- src/components/WelcomeScreen.js
+++ src/components/WelcomeScreen.js
@@ -1,19 +1,36 @@
 import React, { Component } from "react";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
 
-export default class WelcomeScreen extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <p>Welcome to the game!</p>
-
-        <p>
-          You will see two sets of number sequences written in different ways.
-        </p>
-
-        <p>Your task is to recongise each number and give an answer ASAP.</p>
-
-        <p>Are you ready? Click play!</p>
-      </React.Fragment>
-    );
+const useStyles = makeStyles(theme => ({
+  button: {
+    margin: theme.spacing(1)
+  },
+  input: {
+    display: "none"
   }
+}));
+
+export default function WelcomeScreen() {
+  const classes = useStyles();
+
+  return (
+    <React.Fragment>
+      <p>Welcome to the game!</p>
+
+      <p>
+        You will see two sets of number sequences written in different ways.
+      </p>
+
+      <p>Your task is to recongise each number and give an answer ASAP.</p>
+
+      <p>Use your 🠐 🠒 buttons for it.</p>
+
+      <p>Are you ready? Then click play!</p>
+
+      <Button variant="contained" className={classes.button}>
+        Play
+      </Button>
+    </React.Fragment>
+  );
 }
