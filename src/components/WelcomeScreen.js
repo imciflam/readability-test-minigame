256ec65799de5b938a8df5a80e2ed88d1d1205e3
Vita <vitalia@myrambler.ru>
2019-09-28 14:20:51 +0300

    + new component

---
+++ src/components/WelcomeScreen.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class WelcomeScreen extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <p>Welcome to the game!</p>
+      </React.Fragment>
+    );
+  }
+}
