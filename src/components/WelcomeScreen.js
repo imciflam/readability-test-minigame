1e8e5370b3219a93b968b1d9f3c19925371db921
Vita <vitalia@myrambler.ru>
2019-09-28 14:48:22 +0300

    + material ui

--- src/components/WelcomeScreen.js
+++ src/components/WelcomeScreen.js
@@ -5,6 +5,14 @@ export default class WelcomeScreen extends Component {
     return (
       <React.Fragment>
         <p>Welcome to the game!</p>
+
+        <p>
+          You will see two sets of number sequences written in different ways.
+        </p>
+
+        <p>Your task is to recongise each number and give an answer ASAP.</p>
+
+        <p>Are you ready? Click play!</p>
       </React.Fragment>
     );
   }
