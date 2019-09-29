564482bc8d0f43fabe8dd39c764314788c7accc7
Vita <vitalia@myrambler.ru>
2019-09-29 14:13:35 +0300

    Updates

--- precache-manifest.55aa66c48c2aa010fc56135c8d4c166a.js
+++ precache-manifest.55aa66c48c2aa010fc56135c8d4c166a.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a9b61c633d1faebaa6cba0b21e31deee",
+    "revision": "983dea2c1581f4d802b645acd0bc9a32",
     "url": "/readability-test-minigame/index.html"
   },
   {
-    "revision": "85111a06de703701c66c",
+    "revision": "c96c4cd403b0f5be4a02",
     "url": "/readability-test-minigame/static/css/main.5af3311c.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/readability-test-minigame/static/js/2.181caa6d.chunk.js"
   },
   {
-    "revision": "85111a06de703701c66c",
-    "url": "/readability-test-minigame/static/js/main.8b3fee3a.chunk.js"
+    "revision": "c96c4cd403b0f5be4a02",
+    "url": "/readability-test-minigame/static/js/main.24ea5d53.chunk.js"
   },
   {
     "revision": "bea6d1b65b033c42d6f8",
